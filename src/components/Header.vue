483b482c5adce81732bcbdf240bf1e4aa0dc0170
Aditia Kusuma <aditiakusuma800@gmail.com>
2020-06-05 21:55:57 +0700

    create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <h3>Quote Added</h3>
+      <div class="progress" style="height: 20px;">
+        <div
+          class="progress-bar"
+          role="progressbar"
+          :style="{width:( quoteCount/maxQuotes ) * 100 + '%'}"
+        >{{quoteCount}}/{{maxQuotes}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quoteCount", "maxQuotes"]
+};
+</script>
+
+<style>
+</style>
